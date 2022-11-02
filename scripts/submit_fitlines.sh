1907a2448124174a527a7cb7b8aeffdc941c0c30
Erin Kado-Fong <erin.kado-fong@yale.edu>
2022-11-02 10:20:02 -0400

    update singleton batch script

--- scripts/submit_fitlines.sh
+++ scripts/submit_fitlines.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=sedfit
+#SBATCH --job-name=fitlines
 #SBATCH --cpus-per-task=1
-#SBATCH -t 0-0:10:00
-#SBATCH --mem-per-cpu=4G
+#SBATCH -t 0-4:00:00
+#SBATCH --mem-per-cpu=3G
+
 
 module load miniconda
 conda activate vgrace
-python do_fitlines.py -d /home/ek757/surveys/SAGAbg/local_data/ -S 0 -E 10 
+python do_fitlines.py -d /home/ek757/surveys/SAGAbg/local_data/ 
