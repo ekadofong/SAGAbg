8836962cf6349dc4c772c37a8fc0f994131b2985
Erin Kado-Fong <ek757@grace1.grace.hpc.yale.internal>
2022-12-09 16:11:14 -0500

    update args

--- scripts/gDSQ.py
+++ scripts/gDSQ.py
@@ -58,9 +58,9 @@ if __name__ == '__main__':
     #parser.add_argument ( '--nfig', action='store', default=10 )
     args = parser.parse_args ()
     #step = int(args.step)
-    print(args.taskname)
     if args.taskname == 'bayesianfitlines':
         build_bayesianfitlines ( int(args.step),  n_objects=int(args.nobjects), source=args.source, )
-    build ( args.taskname, int(args.step), n_objects=int(args.nobjects), source=args.source, input=args.input,)
+    else:
+        build ( args.taskname, int(args.step), n_objects=int(args.nobjects), source=args.source, input=args.input,)
             #dropbox_directory=args.dropbox_directory )
     #build_dsqQ ( step, n_objects=int(args.nobjects) )
--- scripts/gendsq.sh
+++ scripts/gendsq.sh
@@ -1,1 +1,1 @@
-dsq --job-file "$1" --mem-per-cpu 3000m -t 3:30:00 --partition scavenge --requeue -o logs/dsq-jobfile-%A_%a-%N.out
+dsq --job-file "$1" --mem-per-cpu 3000m -t 5:30:00 --partition scavenge --requeue -o logs/dsq-jobfile-%A_%a-%N.out
